4338aca15487155b62cc26131f12a9d31f48741f
chameleonbr <chameleonbr@gmail.com>
2020-08-16 21:33:18 -0300

    First version with multiple rules per node.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # node-red-contrib-fastest-validator
 Fastest Validator Port for NodeRED
+
+Uses the [Fastest Validator](https://github.com/icebob/fastest-validator) to validate the received message.
+
+You could validate any property of msg, and return of failed validations will be stored on msg.errors property.
+
+Note: You could only write a valid JSON schema to validate your message and JSON form not support js functions(custom fastest validator rules).
+
+This is the first version, I've needed validate various rules at same time and think that we needed to have more performance and personalization, I know that module is the best in performance, now is working with nodered.
---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = function (RED) {
+    "use strict";
+    const Validator = require("fastest-validator");
+    const v = new Validator();
+    
+    function ValidatorFunction(n) {
+        RED.nodes.createNode(this, n);
+        this.props = n.props;
+        this.name = n.name;
+        var node = this;
+
+        for (var i=0,l=n.props.length; i<l; i++) {
+          var value = n.props[i].v ? n.props[i].v : '';
+          var valueType = n.props[i].vt ? n.props[i].vt : 'json';
+          var schema = RED.util.evaluateNodeProperty(value, valueType, this, {});
+          n.props[i].ck = v.compile(schema);
+        }
+
+        node.on("close", function (done) {
+          done();
+        });
+    
+        node.on("input", function (msg) {
+          let checks = [];
+          node.props.forEach(p => {
+            var property = p.p;
+            var check = p.ck;
+            if (!property){
+              checks.push(true);
+              return;
+            }
+            var data = RED.util.getMessageProperty(msg, property);
+            checks.push(check(data));
+          });
+          if(checks.every((el)=>{ return el === true;})){
+              node.send([msg,null]);
+          }else{
+              msg.errors = checks.map((el)=>{if(el === true){
+                return [];
+              }else{
+                return el;
+              }});
+              node.send([null,msg])
+          }
+        });
+      }
+      RED.nodes.registerType("fastest-validator", ValidatorFunction);
+}
